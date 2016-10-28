4a8878ec8a80045710e4bfdd8cd37c82808b49da
Haile Magicho Rekik <a1403844@myy.haaga-helia.fi>
2016-10-28 13:30:02 +0300

    Chapter 6 Securing web App part 2

--- src/main/java/fi/haagahelia/course/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/course/BookstoreApplication.java
@@ -3,7 +3,6 @@ package fi.haagahelia.course;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +12,8 @@ import fi.haagahelia.course.domain.BookstoreRepository;
 import fi.haagahelia.course.domain.Book;
 import fi.haagahelia.course.domain.CategoryRepository;
 import fi.haagahelia.course.domain.Category;
+import fi.haagahelia.course.domain.User;
+import fi.haagahelia.course.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -23,7 +24,7 @@ public class BookstoreApplication {
 	}
 	
 @Bean
-public CommandLineRunner bookDemo(BookstoreRepository repository, CategoryRepository crepository) {
+public CommandLineRunner bookDemo(BookstoreRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of books");
 			
@@ -35,6 +36,12 @@ public CommandLineRunner bookDemo(BookstoreRepository repository, CategoryReposi
 			
 			repository.save(new Book("Title1", "Author1", 1996, "isbn1", 55, crepository.findByName("IT").get(0)));
 			repository.save(new Book("Title2", "Author2", 1999, "isbn2", 65, crepository.findByName("Business").get(0)));	
+			
+			// Create users: admin/password2 user/password1
+				User user1 = new User("user", "$2a$10$X9yO8y..u/Sp5biLLxNZuuBK4.hFWdgPWrlX51ItINEzn/82fPJ2a", "email1", "USER");//password1 bcrypt
+				User user2 = new User("admin", "$2a$10$4KkPnyj.dMMbAIocGkXGcepFnJ61lDefQ7FB8Cwau9gWnRrrKKDei", "email2", "ADMIN"); //password2 bcrypt
+				urepository.save(user1);
+				urepository.save(user2);
 			
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
--- src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -3,20 +3,29 @@ package fi.haagahelia.course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import fi.haagahelia.course.web.UserDetailServiceImpl;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+    private UserDetailServiceImpl userDetailsService;	
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
        // .authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
        // .and()
         .authorizeRequests()
-       // .antMatchers("/", "add", "save", "booklist", "delete/{id}").permitAll()
+        .antMatchers("/", "add", "save", "booklist", "delete/{id}").permitAll()
           .anyRequest().authenticated()
           .and()
       .formLogin()
@@ -28,12 +37,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
           .permitAll();
     }
 
-    @Autowired
+    //using in-memory users in authentication
+    /*@Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER").and()
                 .withUser("admin").password("password").roles("USER", "ADMIN");
+    }*/
+    
+   //using user entities instead of in-memory users in authentication
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
 
